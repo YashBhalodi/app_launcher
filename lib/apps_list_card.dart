5fc314ad7302f6e19498e21dd7793595544a7bdd
YashBhalodi <yashbhalodi007@yahoo.com>
2019-09-19 12:46:19 +0530

    Cleaned up the code a bit

--- lib/apps_list_card.dart
+++ lib/apps_list_card.dart
@@ -7,22 +7,13 @@ import 'package:vibrate/vibrate.dart';
 //TODO repair this file, infinite wait loading the list
 
 class AppsCardList extends StatelessWidget {
-  bool includeAppIcons = true;
+  final bool includeSystemApps;
+  final bool onlyAppsWithLaunchIntent;
 
-  bool includeSystemApps = true;
-  bool onlyAppsWithLaunchIntent = true;
-  double cardHeight = 80.0;
-  double bottomMargin = 10.0;
-  TextStyle textStyle = TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500);
-
-  AppsCardList(
+  const AppsCardList(
       {Key key,
-      this.includeAppIcons,
-      this.includeSystemApps,
-      this.onlyAppsWithLaunchIntent,
-      this.cardHeight,
-      this.bottomMargin,
-      this.textStyle})
+      this.includeSystemApps: false,
+      this.onlyAppsWithLaunchIntent: false})
       : super(key: key);
 
   void _showAcknowledgement(String a) {
@@ -47,86 +38,78 @@ class AppsCardList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: DeviceApps.getInstalledApplications(
-          includeAppIcons: includeAppIcons,
-          onlyAppsWithLaunchIntent: onlyAppsWithLaunchIntent,
-          includeSystemApps: includeSystemApps),
-      builder: (context, appSnap) {
-        switch (appSnap.data) {
-          case null:
-            return Center(child: new CircularProgressIndicator());
-            break;
-          default:
-            {
-              List<Application> appList = appSnap.data;
-              appList.sort((a, b) {
-                return a.appName
-                    .toLowerCase()
-                    .compareTo(b.appName.toLowerCase());
-              });
-              return ListView.builder(
-                  itemCount: appList.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      height: this.cardHeight,
-                      margin: EdgeInsets.only(bottom: this.bottomMargin),
-                      decoration: BoxDecoration(
-                        border: Border.all(width: 0.5),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(5.0),
+        future: DeviceApps.getInstalledApplications(
+            includeAppIcons: true,
+            includeSystemApps: includeSystemApps,
+            onlyAppsWithLaunchIntent: onlyAppsWithLaunchIntent),
+        builder: (context, data) {
+          if (data.data == null) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            List<Application> apps = data.data;
+            return ListView.builder(
+                itemBuilder: (context, position) {
+                  Application app = apps[position];
+                  return Container(
+                    height: 80.0,
+                    margin: EdgeInsets.only(bottom: 10.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 0.5),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5.0),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: <Widget>[
+                        //icon
+                        SizedBox(
+                          height: double.maxFinite,
+                          child: FlutterLogo(
+                            size: 50.0,
+                          ),
                         ),
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        children: <Widget>[
-                          //icon
-                          SizedBox(
-                              height: double.maxFinite,
-                              child: appList[index] is ApplicationWithIcon
-                                  ? FlutterLogo(
-                                      size: cardHeight - 30.0,
-                                    ) //TODO place application icon here
-                                  : FlutterLogo()),
-                          //app name
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.only(left: 10.0),
-                              child: Text(appList[index].appName,
-                                  style: TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.w600,
-                                  )),
-                            ),
+                        //TODO place application icon here
+
+                        //app name
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10.0),
+                            child: Text(app.appName,
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w600,
+                                )),
                           ),
-                          //launch button
-                          MaterialButton(
-                            height: double.maxFinite,
-                            elevation: 5.0,
-                            animationDuration: Duration(milliseconds: 500),
-                            highlightColor: Colors.cyan,
-                            splashColor: Colors.white,
-                            onPressed: () {
-                              Vibrate.feedback(FeedbackType.success);
-                              _showAcknowledgement(appList[index].appName);
-                              DeviceApps.openApp(appList[index].packageName);
-                              SystemChannels.platform
-                                  .invokeMethod('SystemNavigator.pop');
-                            },
-                            color: Colors.blue,
-                            child: Center(
-                                child: Icon(
-                              Icons.launch,
-                              color: Colors.white,
-                              size: 40.0,
-                            )),
-                          ),
-                        ],
-                      ),
-                    );
-                  });
-            }
-        }
-      },
-    );
+                        ),
+                        //launch button
+                        MaterialButton(
+                          height: double.maxFinite,
+                          elevation: 5.0,
+                          animationDuration: Duration(milliseconds: 500),
+                          highlightColor: Colors.cyan,
+                          splashColor: Colors.white,
+                          onPressed: () {
+                            Vibrate.feedback(FeedbackType.success);
+                            _showAcknowledgement(app.appName);
+                            DeviceApps.openApp(app.packageName);
+                            SystemChannels.platform
+                                .invokeMethod('SystemNavigator.pop');
+                          },
+                          color: Colors.blue,
+                          child: Center(
+                              child: Icon(
+                            Icons.launch,
+                            color: Colors.white,
+                            size: 40.0,
+                          )),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                itemCount: apps.length);
+          }
+        });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:device_apps/device_apps.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:vibrate/vibrate.dart';
+import 'package:app_launcher/apps_list_card.dart';
 
 //TODO tidy up the code in separate class file after icon successfully imported
 void main() => runApp(MyApp());
@@ -30,89 +28,41 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    Future getList() async {
-      List<Application> appList = await DeviceApps.getInstalledApplications(
-          includeSystemApps: true,
-          onlyAppsWithLaunchIntent: true,
-          includeAppIcons: true);
-
-      //sorting the list alphabetically
-      appList.sort((a, b) {
-        return a.appName.toLowerCase().compareTo(b.appName.toLowerCase());
-      });
-      return appList;
-    }
-
-    Widget listAppWidget = FutureBuilder(
-      future: getList(),
-      builder: (context, appSnap) {
-        switch (appSnap.connectionState) {
-          case ConnectionState.none:
-            return Center(child: new Text("ConnectionState.none"));
-            break;
-          case ConnectionState.waiting:
-            return Center(
-              child: new CircularProgressIndicator(
-                valueColor: AlwaysStoppedAnimation(Colors.lightBlue),
-              ),
-            );
-            break;
-          case ConnectionState.active:
-            return Center(
-              child: new CircularProgressIndicator(
-                valueColor: AlwaysStoppedAnimation(Colors.green),
-              ),
-            );
-            break;
-          case ConnectionState.done:
-            return ListView.builder(
-                itemCount: appSnap.data.length,
-                itemBuilder: (context, index) {
-                  Application appEntity = appSnap.data[index];
-                  return new AppCard(app: appEntity);
-                });
-            break;
-          default:
-            return Center(child: new CircularProgressIndicator());
-        }
+  Future<bool> _exitPrompt() async {
+    return showDialog<bool>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          title: Text('Oops!'),
+          content: Text("You are trying to exit this app."),
+          actions: <Widget>[
+            RaisedButton(
+              child: Text('Exit!'),
+              onPressed: () {
+                SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+              },
+              color: Colors.red,
+              textColor: Colors.white,
+            ),
+            RaisedButton(
+              child: Text('Stay!'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              color: Colors.blueAccent,
+              textColor: Colors.white,
+            ),
+          ],
+        );
       },
     );
+  }
 
-    Future<bool> _exitPrompt() async {
-      return showDialog<bool>(
-        context: context,
-        barrierDismissible: true,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10.0))),
-            title: Text('Oops!'),
-            content: Text("You are trying to exit this app."),
-            actions: <Widget>[
-              RaisedButton(
-                child: Text('Exit!'),
-                onPressed: () {
-                  SystemChannels.platform.invokeMethod('SystemNavigator.pop');
-                },
-                color: Colors.red,
-                textColor: Colors.white,
-              ),
-              RaisedButton(
-                child: Text('Stay!'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                color: Colors.blueAccent,
-                textColor: Colors.white,
-              ),
-            ],
-          );
-        },
-      );
-    }
-
+  @override
+  Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: _exitPrompt,
       child: Scaffold(
@@ -131,101 +81,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
           title: Text(widget.title),
         ),
-        body: listAppWidget,
-      ),
-    );
-  }
-}
-
-class AppCard extends StatelessWidget {
-  AppCard({Key key, this.app}) : super(key: key);
-  final Application app;
-
-  _showAcknowledgement(String a) {
-    //toast acknowledgement until SimpleDialog is repaired.
-
-    Fluttertoast.showToast(
-        msg: "Closing the launcher\nLaunching $a",
-        toastLength: Toast.LENGTH_LONG,
-        gravity: ToastGravity.BOTTOM,
-        fontSize: 16.0);
-
-    //possible bug in following code block
-    //The next toast has unused text below the msg
-    //TODO figure out the solution of extra space in toast
-    /*Future.delayed(Duration(milliseconds: 1000),(){
-      Fluttertoast.cancel();
-      Fluttertoast.showToast(
-        msg: "Launching $a\n",
-        toastLength: Toast.LENGTH_LONG,
-        gravity: ToastGravity.BOTTOM,
-        fontSize: 16.0);});*/
-
-    //intended SimpleDialog acknowledgement
-    /*return new SimpleDialog(
-      title: new Text("Roger that!"),
-      children: <Widget>[
-        Container(child: new Text("Launching $a"),),
-        Container(child: new CircularProgressIndicator(),)
-      ],
-    );*/
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 80.0,
-      margin: EdgeInsets.only(bottom: 10.0),
-      decoration: BoxDecoration(
-        border: Border.all(width: 0.5),
-        borderRadius: BorderRadius.all(
-          Radius.circular(5.0),
-        ),
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          //icon
-          SizedBox(
-              height: double.maxFinite,
-              child: FlutterLogo(
-                size: 50.0,
-              )),
-          //TODO place app's icon here
-          //app name
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 10.0),
-              child: Text(app.appName,
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.w600,
-                  )),
-            ),
-          ),
-          //launch button
-          MaterialButton(
-            height: double.maxFinite,
-            elevation: 5.0,
-            animationDuration: Duration(milliseconds: 500),
-            highlightColor: Colors.cyan,
-            splashColor: Colors.white,
-            onPressed: () {
-              //TODO repair acknowledgement prompt
-              Vibrate.feedback(FeedbackType.success);
-              _showAcknowledgement(app.appName);
-              DeviceApps.openApp(app.packageName);
-              SystemChannels.platform.invokeMethod('SystemNavigator.pop');
-            },
-            color: Colors.blue,
-            child: Center(
-                child: Icon(
-              Icons.launch,
-              color: Colors.white,
-              size: 40.0,
-            )),
-          ),
-        ],
+        body: AppsCardList(),
       ),
     );
   }
