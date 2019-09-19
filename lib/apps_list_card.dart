8ee4ef2316e05301c476f9ad72acaf8b5a704782
YashBhalodi <yashbhalodi007@yahoo.com>
2019-09-19 13:14:47 +0530

    Removed the clutter, updated TODO list

--- lib/apps_list_card.dart
+++ lib/apps_list_card.dart
@@ -4,8 +4,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:vibrate/vibrate.dart';
 
-//TODO repair this file, infinite wait loading the list
-
 class AppsCardList extends StatelessWidget {
   final bool includeSystemApps;
   final bool onlyAppsWithLaunchIntent;
@@ -30,22 +28,11 @@ class AppsCardList extends StatelessWidget {
       : super(key: key);
 
   void _showAcknowledgement(String a) {
-    //toast acknowledgement until SimpleDialog is repaired.
-
     Fluttertoast.showToast(
         msg: "Closing the launcher\nLaunching $a",
         toastLength: Toast.LENGTH_LONG,
         gravity: ToastGravity.BOTTOM,
         fontSize: 16.0);
-
-    //intended SimpleDialog acknowledgement
-    /*return new SimpleDialog(
-      title: new Text("Roger that!"),
-      children: <Widget>[
-        Container(child: new Text("Launching $a"),),
-        Container(child: new CircularProgressIndicator(),)
-      ],
-    );*/
   }
 
   @override
@@ -68,63 +55,64 @@ class AppsCardList extends StatelessWidget {
               });
             }
             return ListView.builder(
-                itemBuilder: (context, position) {
-                  Application app = apps[position];
-                  return Container(
-                    height: this.cardHeight,
-                    margin: EdgeInsets.only(bottom: this.cardMarginBottom),
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 0.5),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(5.0),
-                      ),
+              itemCount: apps.length,
+              itemBuilder: (context, position) {
+                Application app = apps[position];
+                return Container(
+                  height: this.cardHeight,
+                  margin: EdgeInsets.only(bottom: this.cardMarginBottom),
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 0.5),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5.0),
                     ),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.max,
-                      children: <Widget>[
-                        //icon
-                        SizedBox(
-                            height: double.maxFinite,
-                            width: this.cardHeight,
-                            child: app is ApplicationWithIcon && getIcons
-                                ? Image.memory(app.icon)
-                                : FlutterLogo(size: this.cardHeight)),
-                        //TODO place application icon here
-
-                        //app name
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 10.0),
-                            child: Text(app.appName, style: this.appNameStyle),
-                          ),
-                        ),
-                        //launch button
-                        MaterialButton(
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      //icon
+                      SizedBox(
                           height: double.maxFinite,
-                          elevation: 5.0,
-                          animationDuration: Duration(milliseconds: 500),
-                          highlightColor: Colors.cyan,
-                          splashColor: Colors.white,
-                          onPressed: () {
-                            Vibrate.feedback(FeedbackType.success);
-                            _showAcknowledgement(app.appName);
-                            DeviceApps.openApp(app.packageName);
-                            SystemChannels.platform
-                                .invokeMethod('SystemNavigator.pop');
-                          },
-                          color: Colors.blue,
-                          child: Center(
-                              child: Icon(
-                            Icons.launch,
-                            color: Colors.white,
-                            size: 40.0,
-                          )),
+                          width: this.cardHeight,
+                          child: app is ApplicationWithIcon && getIcons
+                              ? Image.memory(app.icon)
+                              : FlutterLogo(size: this.cardHeight)),
+
+                      //app name
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 10.0),
+                          child: Text(app.appName, style: this.appNameStyle),
                         ),
-                      ],
-                    ),
-                  );
-                },
-                itemCount: apps.length);
+                      ),
+
+                      //launch button
+                      MaterialButton(
+                        height: double.maxFinite,
+                        elevation: 5.0,
+                        animationDuration: Duration(milliseconds: 500),
+                        highlightColor: Colors.cyan,
+                        splashColor: Colors.white,
+                        onPressed: () {
+                          Vibrate.feedback(FeedbackType.success);
+                          _showAcknowledgement(app.appName);
+                          DeviceApps.openApp(app.packageName);
+                          SystemChannels.platform
+                              .invokeMethod('SystemNavigator.pop');
+                        },
+                        color: Colors.blue,
+                        child: Center(
+                            child: Icon(
+                          Icons.launch,
+                          color: Colors.white,
+                          size: 40.0,
+                        )),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
           }
         });
   }
